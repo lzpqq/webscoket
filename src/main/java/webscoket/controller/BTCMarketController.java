21b0bbabe39b9b7ccef2c116420e342200afd463
lianzhaopei <lianzhaopei@163.com>
2018-10-12 19:11:37 +0800

    市场1

--- src/main/java/webscoket/controller/BTCMarketController.java
+++ src/main/java/webscoket/controller/BTCMarketController.java
@@ -5,6 +5,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import webscoket.domain.ResponseBo;
 import webscoket.service.IBTCMarketApi;
@@ -20,7 +22,7 @@ public class BTCMarketController {
     @Autowired
     private IBTCMarketApi ibtcMarketApi;
 
-    @RequestMapping("/listMarket")
+    @RequestMapping(value = "/listMarket",method = RequestMethod.GET)
     public ResponseBo listMarket(){
         String btcMarket = ibtcMarketApi.getBTCMarket();
 
@@ -30,4 +32,26 @@ public class BTCMarketController {
         responseBo.setData(data);
         return  responseBo;
     }
+
+    @RequestMapping(value = "/order",method = RequestMethod.POST)
+    public ResponseBo order(@RequestParam("tradeId") String tradeId, @RequestParam("amount")String amount){
+        String order = ibtcMarketApi.order(tradeId, amount);
+        JSONObject jsonObject = JSON.parseObject(order);
+        ResponseBo responseBo = new ResponseBo();
+        responseBo.setMsg((String) jsonObject.get("message"));
+        responseBo.setData(jsonObject.get("data"));
+        return  responseBo;
+    }
+
+    @RequestMapping(value = "/orderInfo",method = RequestMethod.GET)
+    public ResponseBo orderInfo(@RequestParam("number") String number){
+        String orderInfo = ibtcMarketApi.orderInfo(number);
+        JSONObject jsonObject = JSON.parseObject(orderInfo);
+        Object data = jsonObject.get("data");
+        ResponseBo responseBo = new ResponseBo();
+        responseBo.setData(data);
+        return  responseBo;
+    }
+
+
 }
--- src/main/java/webscoket/service/impl/BTCMarketApi.java
+++ src/main/java/webscoket/service/impl/BTCMarketApi.java
@@ -3,9 +3,13 @@ package webscoket.service.impl;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import webscoket.service.IBTCMarketApi;
+
+import java.util.HashMap;
 
 /**
  * @author pc
@@ -17,16 +21,57 @@ public class BTCMarketApi  implements IBTCMarketApi {
     @Value("${huobi.token}")
     private String token;
 
+    @Value("${huobi.pay.pasd}")
+    private String password;
 
+    /**
+     * 市场行情
+     * @return
+     */
     @Override
     public String getBTCMarket() {
 
-        HttpRequest get = HttpUtil.createGet("https://otc-api.eiijo.cn/v1/data/trade-market?country=37&currency=1&payMethod=0&currPage=1&coinId=2&tradeType=sell&blockType=general&online=1");
+        HttpRequest get = HttpUtil.createGet("https://otc-api.hbg.com/v1/data/trade-market?country=37&currency=1&payMethod=0&currPage=1&coinId=2&tradeType=sell&blockType=general&online=1");
         HttpRequest token = get.header("token", this.token);
         HttpResponse response = token.execute();
         String body = response.body();
         return body;
-
-
     }
+
+    /**
+     * 下单
+     *
+     */
+    @Override
+    public String order(String tradeId, String amount) {
+        HttpRequest get = HttpUtil.createGet("https://otc-api.eiijo.cn/v1/otc/order/ticket?tradeId=" + tradeId);
+        HttpRequest request = get.header("token", this.token);
+        HttpResponse response = request.execute();
+        String body = response.body();
+        JSONObject jsonObject = JSON.parseObject(body);
+        JSONObject data = jsonObject.getJSONObject("data");
+        String ticket = (String) data.get("ticket");
+
+        HttpRequest post = HttpUtil.createPost("https://otc-api.hbg.com/v1/otc/order");
+        HttpRequest token = post.header("token", this.token);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("amount",amount);
+        map.put("tradeId",tradeId);
+        map.put("ticket",ticket);
+        map.put("password",this.password);
+        HttpRequest form = token.form(map);
+        HttpResponse execute = form.execute();
+        String body1 = execute.body();
+        return body1;
+    }
+
+    @Override
+    public String orderInfo(String number) {
+        HttpRequest get = HttpUtil.createGet("https://otc-api.hbg.com/v1/otc/order/"+number);
+        HttpRequest token = get.header("token", this.token);
+        HttpResponse response = token.execute();
+        String body = response.body();
+        return body;
+    }
+
 }
